2db41bed923307beb2addf445ffc262f81746beb
TLSenZ <sven@zemp.email>
2025-06-25 21:57:05 +0200

    Working on JWT

--- src/Security/JWT.rs
+++ src/Security/JWT.rs
@@ -1,10 +1,17 @@
+use crate::Security::JWT::AuthError::AuthError;
 use std::env;
+use std::fmt::Error;
+use axum::{http, Error};
+use axum::http::{HeaderValue, Response, StatusCode};
+use axum::extract::Request;
+use axum::middleware::Next;
 use dotenv::dotenv;
 use jsonwebtoken::{encode, Algorithm, EncodingKey, Header, Validation, decode, DecodingKey, TokenData};
-use crate::model::securitymodel::EncodeJWT;
+use crate::model::securitymodel::{AuthError, EncodeJWT};
+use crate::model::securitymodel::AuthError::AuthError;
 use crate::model::usermodel::ConversionError;
 
-pub async fn encode_jwt(name: &str, email: &str) -> Result<String, ConversionError>{
+pub fn encode_jwt(name: &str, email: &str) -> Result<String, ConversionError>{
 
     let jwt_info = EncodeJWT {
         username: name.to_string(),
@@ -17,10 +24,24 @@ pub async fn encode_jwt(name: &str, email: &str) -> Result<String, ConversionErr
     Ok(token)
 }
 
-pub async fn decode_jwt(jwt_token: String)->  Result<TokenData<EncodeJWT>, ConversionError>{
+pub fn decode_jwt(jwt_token: String)->  Result<TokenData<EncodeJWT>, ConversionError>{
 
     dotenv().ok();
     let secret = env::var("JWT_SECRET")?;
     let token_message = decode::<EncodeJWT>(&jwt_token, &DecodingKey::from_secret(secret.as_ref()), &Validation::new(Algorithm::HS256)).unwrap();
     Ok(token_message)
+}
+
+pub async fn authenticate(mut req:Request, next: Next ) -> Result<Response<Header>, AuthError>{
+    let auth_header = req.headers().get(http::header::AUTHORIZATION);
+    let auth_header = match auth_header {
+        Some(header) => { header.to_str().map_err(|_| AuthError("Empty header is not allowed".to_string(), StatusCode::FORBIDDEN))},
+        None => { Err(AuthError("Please add JWT to your Header".to_string(), StatusCode::FORBIDDEN)) }
+    }?;
+    
+    let mut header = auth_header.split_whitespace();
+    let (bearer, token) = (header.next(), header.next());
+    let token_data = decode_jwt(token.unwrap().to_string());
+    
+    todo!()
 }
--- src/model/securitymodel.rs
+++ src/model/securitymodel.rs
@@ -1,4 +1,8 @@
+use std::fmt;
+use std::fmt::{write, Formatter};
+use axum::http::StatusCode;
 use serde::{Deserialize, Serialize};
+use crate::model::usermodel::ConversionError;
 
 #[derive(Deserialize, Serialize)]
 pub struct EncodeJWT{
@@ -6,3 +10,20 @@ pub struct EncodeJWT{
     pub(crate) email: String
 }
 
+#[derive(Debug)]
+pub enum AuthError {
+    AuthError(String,StatusCode)
+}
+
+impl fmt::Display for AuthError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            AuthError::AuthError(message, status) => write!(f,"Error: {},StatusCode: {}", message, status)
+        }
+        
+    }
+}
+
+
+impl std::error::Error for AuthError{
+}
